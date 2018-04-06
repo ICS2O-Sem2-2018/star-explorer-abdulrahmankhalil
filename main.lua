5e29ee95b3e8c93538d48029562078ab72a53e43
abdulrahmankhalil <36486514+abdulrahmankhalil@users.noreply.github.com>
2018-04-06 13:21:34 -0400

    Highschores work

--- main.lua
+++ main.lua
@@ -1,7 +1,16 @@
 -----------------------------------------------------------------------------------------
---
+local composer = require ( "composer" )
 -- main.lua
---
+local scene = composer.newScene()
 -----------------------------------------------------------------------------------------
 
--- Your code here
+
+
+-- Hide status bar
+display.setStatusBar( display.HiddenStatudBar )
+
+-- seed the random number generator
+math.randomseed( os.time() )
+
+-- Go to the menu screen
+composer.gotoScene( "menu" )
